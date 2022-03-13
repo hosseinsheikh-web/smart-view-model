6349cc3505402882377c71f97de6aaa35adbafb0
Hossein <hosseinsheikh.web@gmail.com>
2022-03-13 15:12:47 +0330

    refactor(MethodParameter): Refactor get method in MethodParameter class and check type of view model method arguments and return throw if types are not match

--- src/MethodParameter.php
+++ src/MethodParameter.php
@@ -6,14 +6,34 @@ use HosseinSheikh\ViewModel\Contracts\MethodParameterContract;
 
 class MethodParameter implements MethodParameterContract
 {
+
+    /**
+     * @param $viewModel
+     * @param $actionMethod
+     * @return array
+     * @throws \ReflectionException
+     */
     public static function get($viewModel, $actionMethod)
     {
-        $viewModelReflection = new \ReflectionObject($viewModel);
+        $viewModelReflection = new \ReflectionClass($viewModel);
         $maybeEncodeReflection = $viewModelReflection->getMethod($actionMethod);
         $maybeEncodeReflection->setAccessible(true);
         $parameters = [];
         foreach ($maybeEncodeReflection->getParameters() as $method) {
-            $parameters[] = resolve($method->getClass()->getName());
+            if ($method->getType() && class_exists($method->getType()->getName())) {
+                $parameters[] = resolve($method->getType()->getName());
+            } elseif ($method->getType() && ! class_exists($method->getType()->getName())) {
+                if (gettype(request()->{$method->getName()}) == $method->getType()->getName()) {
+                    $parameters[] = request()->{$method->getName()};
+                } else {
+                    $urlType = gettype(request()->{$method->getName()});
+                    $methodVarType = $method->getType()->getName();
+
+                    throw new \TypeError("Type of {$method->getName()} ( $urlType ) is not match with ( $methodVarType ) type of method argument");
+                }
+            } else {
+                $parameters[] = request()->{$method->getName()};
+            }
         }
 
         return $parameters;
